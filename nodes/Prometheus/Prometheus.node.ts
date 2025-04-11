b4177e13a7ef10fdf4f32f016533264ddbf19f3a
Yoav <yoava554@gmail.com>
2025-04-11 15:26:05 +0300

    basic version - WIP

---
+++ nodes/Prometheus/Prometheus.node.ts
@@ -0,0 +1,98 @@
+import { INodeType, INodeTypeDescription, NodeConnectionType, 	 } from 'n8n-workflow';
+
+export class Prometheus implements INodeType {
+	description: INodeTypeDescription = {
+		// Basic node details will go here
+		displayName: 'PrometheusAPI',
+		name: 'prometheus',
+		icon: 'file:Prometheus.svg',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+		description: 'Query data from Prometheus.',
+		defaults: {
+			name: 'Prometheus Query',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		requestDefaults: {
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+			method: 'GET',
+			url: '/api/v1/query',
+		},
+
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Prometheus',
+						value: 'prometheus',
+					},
+					{
+						// eslint-disable-next-line n8n-nodes-base/node-param-resource-with-plural-option
+						name: 'Thanos',
+						value: 'thanos',
+					}
+				],
+				default: 'thanos',
+			},
+
+			{
+				displayName: 'URL',
+				description: 'Prometheus / Thanos base URL',
+				required: true,
+				name: 'url',
+				type: 'string',
+				default:'',
+				displayOptions: {
+					show: {
+						resource: [
+							'prometheus',
+							'thanos',
+						],
+					},
+				},
+				routing: {
+					request: {
+						baseURL: '={{ $value }}',
+					},
+				},
+			},
+
+			// Operations will go here (required fields)
+			{
+				displayName: 'Query',
+				description: 'PromQL query',
+				required: true,
+				name: 'promqLquery',
+				type: 'string',
+				default:'',
+				displayOptions: {
+					show: {
+						resource: [
+							'prometheus',
+							'thanos',
+						],
+					},
+				},
+				routing: {
+					request: {
+						// You've already set up the URL. qs appends the value of the field as a query string
+						qs: {
+							query: '={{ $value }}',
+						},
+					},
+				},
+			},
+			// Optional/additional fields will go here
+		]
+
+	};
+}
